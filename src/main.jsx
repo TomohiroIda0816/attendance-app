c94ea16759e19775fa4c4d6e45e54bda665b5df2
TomohiroIda0816 <TomohiroIda0816@users.noreply.github.com>
2026-02-15 21:14:40 +0900

    auto cleanup broken sessions

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+﻿import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AuthProvider } from './components/AuthProvider';
 import App from './App';
 import './index.css';
 
+// 壊れたセッションを自動クリーンアップ
+try {
+  var keys = Object.keys(localStorage);
+  for (var i = 0; i < keys.length; i++) {
+    if (keys[i].includes('supabase') && keys[i].includes('auth')) {
+      try {
+        var val = JSON.parse(localStorage.getItem(keys[i]));
+        if (!val || !val.access_token) {
+          localStorage.removeItem(keys[i]);
+        }
+      } catch(e) {
+        localStorage.removeItem(keys[i]);
+      }
+    }
+  }
+} catch(e) {}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
+  React.createElement(React.StrictMode, null,
+    React.createElement(AuthProvider, null,
+      React.createElement(App, null)
+    )
+  )
 );
